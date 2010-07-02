2f011128734d2d40f6eb2fbc5edde8c7af3bb3b9
Jimmy Schementi <jschementi@gmail.com>
2010-07-02 00:49:43 -0700

    Tiny cleanup of sinatra app, and update TODO

--- app.rb
+++ app.rb
@@ -6,18 +6,13 @@ require 'sinatra'
 require 'json'
 
 get '/' do
-  File.open('public/index.html'){|f| f.read}
+  File.open('public/index.html'){ |f| f.read }
 end
 
 get '/blog.rss' do
   content_type 'application/xml', :charset => 'utf-8'
-  url = URI.parse('http://feeds.feedburner.com/jimmy-thinking')
-  res = Net::HTTP.start(url.host, url.port) {|http|
+  res = Net::HTTP.start("feeds.feedburner.com", 80) { |http|
     http.get('/jimmy-thinking')
   }
   res.body
-end
-
-get '/flickr.rss' do
-
 end
